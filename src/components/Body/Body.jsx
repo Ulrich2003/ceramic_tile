25b66e3127097125c98edcfd913910cfb3b58487
ChuanyangChen <whistler2003@outlook.com>
2022-06-13 14:24:29 +0800

    backup

---
+++ src/components/Body/Body.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ImgCard } from '../ImgCard/ImgCard'
+import './Body.css'
+
+export const Body = () => {
+  return (
+    <div>
+      <ImgCard/>
+    </div>
+  )
+}
---
+++ src/components/ImgCard/ImgCard.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ImgCard.css'
+
+export const ImgCard = () => {
+  return (
+    <div className='ImgCard'>
+      <img src="https://images.unsplash.com/photo-1638913971251-832d29947de6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80" alt="" />
+    </div>
+  )
+}
