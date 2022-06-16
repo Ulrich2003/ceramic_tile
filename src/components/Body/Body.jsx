e1e9ad44a3a2ad772951332590d5924cc6e904a1
ChuanyangChen <whistler2003@outlook.com>
2022-06-16 19:40:09 +0800

    in trouble

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import { ImgCard } from '../ImgCard/ImgCard'
-import './Body.css'
+import React from "react";
+import { ImgCard } from "../ImgCard/ImgCard";
+import "./Body.css";
 
 export const Body = () => {
+  let dataList = [
+    {
+      id: 0,
+      content:
+        "这是一条很长很长很长很长很长很长长很长很长长很长很长长很长很长长很长很长很长很长很长的描述",
+      src: "https://images.unsplash.com/photo-1641382368446-9ec0f0fb9429?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80",
+    },
+  ];
+
   return (
     <div>
-      <ImgCard/>
+      <ul>
+        {dataList.map((item,index) => {
+          console.log(item);
+          <li>
+            <ImgCard data={dataList[index]} />;
+          </li>;
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
--- src/components/ImgCard/ImgCard.jsx
+++ src/components/ImgCard/ImgCard.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import './ImgCard.css'
+import React from "react";
+import "./ImgCard.css";
 
-export const ImgCard = () => {
+export const ImgCard = (props) => {
+
+  let {content,src} = props.data
+  console.log(1,props);
   return (
-    <div className='ImgCard'>
-      <img src="https://images.unsplash.com/photo-1638913971251-832d29947de6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80" alt="" />
+    <div className="ImgCard">
+      <img src={src} />
+      <p>
+        {content}
+      </p>
     </div>
-  )
-}
+  );
+};
